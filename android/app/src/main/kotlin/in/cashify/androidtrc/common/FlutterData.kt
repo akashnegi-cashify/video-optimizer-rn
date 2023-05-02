e784085d444fd43cc20f75956017111d1e6ee7e4
Vipin <vipin.g@cashify.in>
2023-05-02 22:47:07 +0530

    remove android module

--- android/app/src/main/kotlin/in/cashify/androidtrc/common/FlutterData.kt
+++
@@ -1,8 +0,0 @@
-package `in`.cashify.androidtrc.common
-
-import com.google.gson.annotations.SerializedName
-
-data class FlutterData(
-    @SerializedName("token"    ) var token    : String?   = null,
-    @SerializedName("authData" ) var authData : AuthData? = AuthData()
-)
