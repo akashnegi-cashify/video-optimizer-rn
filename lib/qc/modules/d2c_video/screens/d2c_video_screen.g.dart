3ea7911fac3007a9e5a59fff91c5badea29d57d6
Vipin <vipin.g@cashify.in>
2024-05-29 14:29:35 +0530

    auto generate file

---
+++ lib/qc/modules/d2c_video/screens/d2c_video_screen.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'd2c_video_screen.dart';
+
+// **************************************************************************
+// Generator: PageSchemaGen
+// **************************************************************************
+
+dynamic schema() => {
+      //#adminscreen
+      "type": "@@page",
+      "key": "QC_d2c_video_page_key",
+      "pageGroup": "QC D2C Video",
+      "title": "D2 C Video Screen",
+      "isActive": true,
+      "params": [
+        {"key": "dbr", "value": null}
+      ]
+
+      //#adminscreen
+    };
