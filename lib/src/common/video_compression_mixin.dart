0e8a0bdd6bf8734bbebe2536ee51f05c8cf6e4fc
Vipin <vipin.g@cashify.in>
2024-08-30 11:08:43 +0530

    SCM-7277, add retry compression mechanism

--- lib/src/common/video_compression_mixin.dart
+++ lib/src/common/video_compression_mixin.dart
@@ -1,27 +1,50 @@
 import 'dart:async';
 
-import 'package:core/core.dart';
 import 'package:core_widgets/core_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_trc/src/common/utils/video_util.dart';
+
+extension RetryFuture<T> on Future<T> {
+  Future<T> retry(int maxRetries, {Duration? duration}) {
+    var completer = Completer<T>();
+    var retries = 0;
+    void retry() {
+      then((value) {
+        completer.complete(value);
+      }).catchError((error) {
+        if (retries < maxRetries) {
+          retries++;
+          Future.delayed(duration ?? Duration.zero, retry);
+        } else {
+          completer.completeError(error);
+        }
+      });
+    }
+
+    retry();
+    return completer.future;
+  }
+}
 
 mixin VideoCompressionMixin {
   final StreamController<int> _fileCompressProgressStream = StreamController.broadcast();
 
   Future<String> compressVideo(String filePath, int videoTimeInSec) {
     var completer = Completer<String>();
-    VideoUtil.compressVideo(filePath, videoTimeInSec, onProgress: (value) {
+    void progress(int value) {
       _fileCompressProgressStream.add(value);
-    }).then((String? outputPath) {
+    }
+
+    _withRetry(() => _compressVideo(filePath, videoTimeInSec, progress), 3).then((outputPath) {
       if (outputPath != null) {
         completer.complete(outputPath);
       } else {
         completer.completeError('Error while compressing video');
       }
     }, onError: (error) {
-      Logger.debug('mydebug-----VideoCompressionMixin.compressVideo', ['error', error]);
       completer.completeError(error.toString());
     });
+
     return completer.future;
   }
 
@@ -58,5 +81,26 @@ mixin VideoCompressionMixin {
         );
       },
     );
+  }
+
+  Future<String?> _withRetry(Future<dynamic> Function() callableFunction, int maxRetry,
+      {Duration duration = const Duration(seconds: 2)}) async {
+    for (int attempt = 0; attempt < maxRetry; attempt++) {
+      try {
+        String? outputPath = await callableFunction();
+        return Future.value(outputPath);
+      } catch (error) {
+        if (attempt >= (maxRetry -1)) {
+          return Future.error(error.toString());
+        }
+        await Future.delayed(duration);
+        continue;
+      }
+    }
+    return Future.error("error");
+  }
+
+  Future<String?> _compressVideo(String filePath, int videoTimeInSec, ValueChanged<int> progress) {
+    return VideoUtil.compressVideo(filePath, videoTimeInSec, onProgress: progress);
   }
 }
